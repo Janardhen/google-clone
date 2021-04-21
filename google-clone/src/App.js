6d610a141ccef523234365a3c3618b100144ac7f
Janardhen <52244557+Janardhen@users.noreply.github.com>
2021-04-21 00:53:59 -0400

    Header and Google image added

--- google-clone/src/App.js
+++ google-clone/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Testing google clone</h1>
-      <Home />
+      <Router> 
+      <Switch>
+          <Route path="/search">
+          <h1>This is search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        </Router>
     </div>
   );
 }
